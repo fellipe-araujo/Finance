6c8d70351b80b85f3ada2062859762af313174e0
Fellipe <araujo.fellipe@hotmail.com>
2021-01-27 13:50:01 -0300

    #4 Create Category Controller with methods: index, show and store.

--- backend/src/controllers/CategoryController.ts
+++ backend/src/controllers/CategoryController.ts
@@ -1,7 +1,27 @@
 import { Request, Response } from 'express';
+import Category from '../models/Category';
 
 export default {
   async index(request: Request, response: Response) {
-    response.json({ message: 'categories' });
+    const categories = await Category.find();
+
+    return response.json(categories);
+  },
+
+  async show(request: Request, response: Response) {
+    const category = await Category.findById(request.params.categoryId);
+
+    return response.json(category);
+  },
+
+  async store(request: Request, response: Response) {
+    const { name, color } = request.body;
+
+    const category = await Category.create({
+      name,
+      color,
+    });
+
+    return response.status(201).json(category);
   },
 };
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import UserController from './controllers/UserController';
 import AuthController from './controllers/AuthController';
 import FinanceController from './controllers/FinanceController';
+import CategoryController from './controllers/CategoryController';
 
 import authMiddleware from './middlewares/auth';
 
@@ -14,8 +15,13 @@ routes.post('/register', UserController.store);
 routes.post('/authenticate', AuthController.store);
 routes.get('/users/all', UserController.index);
 routes.get('/users/:_id', UserController.show);
+
 routes.post('/finances', FinanceController.store);
 routes.get('/finances/all', FinanceController.index);
 routes.get('/finances/:financeId', FinanceController.show);
+
+routes.post('/categories', CategoryController.store);
+routes.get('/categories/all', CategoryController.index);
+routes.get('/categories/:categoryId', CategoryController.show);
 
 export default routes;
