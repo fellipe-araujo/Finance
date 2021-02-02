322d9df104dc229747e1208d8cc5ecbb363fc939
Fellipe <araujo.fellipe@hotmail.com>
2021-02-01 21:34:52 -0300

    #14 Add principal pages with default structure.

---
+++ mobile/src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import styles from './styles';
+
+const Home = () => (
+  <View style={styles.container}>
+    <Text>Home</Text>
+  </View>
+);
+
+export default Home;
