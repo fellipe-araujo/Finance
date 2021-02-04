e44a3f7b021afa1b37c0ca1deb2d21bcc87ec2d6
Fellipe <araujo.fellipe@hotmail.com>
2021-02-03 21:47:57 -0300

    #18 Separate routes that need authentication.

---
+++ mobile/src/routes/app.routes.tsx
@@ -0,0 +1,137 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MaterialIcons as Icon } from '@expo/vector-icons';
+import { LinearGradient } from 'expo-linear-gradient';
+
+import Account from '../pages/Account';
+import Category from '../pages/Category';
+import Transaction from '../pages/Transaction';
+import Home from '../pages/Home';
+import Objective from '../pages/Objective';
+
+const AccountStack = createStackNavigator();
+
+const AccountScreen: React.FC = () => (
+  <AccountStack.Navigator headerMode={'none'}>
+    <AccountStack.Screen name="Account" component={Account} />
+  </AccountStack.Navigator>
+);
+
+const CategoryStack = createStackNavigator();
+
+const CategoryScreen: React.FC = () => (
+  <CategoryStack.Navigator headerMode={'none'}>
+    <CategoryStack.Screen name="Category" component={Category} />
+  </CategoryStack.Navigator>
+);
+
+const TransactionStack = createStackNavigator();
+
+const TransactionScreen: React.FC = () => (
+  <TransactionStack.Navigator headerMode={'none'}>
+    <TransactionStack.Screen name="Transaction" component={Transaction} />
+  </TransactionStack.Navigator>
+);
+
+const HomeStack = createStackNavigator();
+
+const HomeScreen: React.FC = () => (
+  <HomeStack.Navigator headerMode={'none'}>
+    <HomeStack.Screen name="Home" component={Home} />
+  </HomeStack.Navigator>
+);
+
+const ObjectiveStack = createStackNavigator();
+
+const ObjectiveScreen: React.FC = () => (
+  <ObjectiveStack.Navigator headerMode={'none'}>
+    <ObjectiveStack.Screen name="Objective" component={Objective} />
+  </ObjectiveStack.Navigator>
+);
+
+const Tab = createBottomTabNavigator();
+
+const Routes: React.FC = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
+
+          switch (route.name) {
+            case 'Home':
+              iconName = 'home';
+              break;
+            case 'Account':
+              iconName = 'credit-card';
+              break;
+            case 'Transaction':
+              iconName = 'add';
+              break;
+            case 'Objective':
+              iconName = 'flag';
+              break;
+            case 'Category':
+              iconName = 'category';
+              break;
+            default:
+              iconName = 'circle';
+              break;
+          }
+
+          return <Icon name={iconName} size={size} color={color} />;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: '#B8C0FF',
+        inactiveTintColor: '#fff',
+        showLabel: false,
+        style: { backgroundColor: '#39393a' },
+      }}
+    >
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Account" component={AccountScreen} />
+      <Tab.Screen
+        name="Transaction"
+        component={TransactionScreen}
+        options={() => ({
+          tabBarIcon: () => (
+            <View style={styles.iconTabRound}>
+              <LinearGradient
+                style={styles.iconTabRound}
+                start={{ x: 0.3, y: 0.3 }}
+                end={{ x: 1, y: 1 }}
+                colors={['#B9C0FF', '#42A1DC']}
+              >
+                <Icon name="add" size={26} color="#FFF" />
+              </LinearGradient>
+            </View>
+          ),
+        })}
+      />
+      <Tab.Screen name="Objective" component={ObjectiveScreen} />
+      <Tab.Screen name="Category" component={CategoryScreen} />
+    </Tab.Navigator>
+  );
+};
+
+const styles = StyleSheet.create({
+  iconTabRound: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    marginBottom: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    elevation: 6,
+    shadowColor: '#9C27B0',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 5,
+  },
+});
+
+export default Routes;
