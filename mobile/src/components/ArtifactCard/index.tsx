1a8dee688b07e52180575e9bf849c580601a28fe
Fellipe <araujo.fellipe@hotmail.com>
2021-02-06 00:49:06 -0300

    #20 Create ArtifactCard component.

---
+++ mobile/src/components/ArtifactCard/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import styles from './styles';
+
+interface Props {
+  colorLinearX: string;
+  colorLinearY: string;
+  borderTopColor: string;
+  colorTotal: string;
+  artifactName: string;
+}
+
+const ArtifactCard: React.FC<Props> = ({
+  colorLinearX,
+  colorLinearY,
+  borderTopColor,
+  colorTotal,
+  artifactName
+}) => {
+  return (
+    <LinearGradient
+      style={styles.container}
+      start={{ x: 0.3, y: 0.3 }}
+      end={{ x: 1, y: 1 }}
+      colors={[colorLinearX, colorLinearY]}
+    >
+      <Text style={styles.title}>{artifactName}</Text>
+      <View
+        style={[styles.quantityContainer, { borderTopColor: borderTopColor }]}
+      >
+        <Text style={[styles.total, { color: colorTotal }]}>Total:</Text>
+        <Text style={[styles.total, { color: colorTotal }]}>5</Text>
+      </View>
+    </LinearGradient>
+  );
+};
+
+export default ArtifactCard;
