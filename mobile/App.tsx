b415389643edb5007cd47d73e389fc5d1b74d656
Fellipe <araujo.fellipe@hotmail.com>
2021-02-06 00:47:47 -0300

    #20 Add and config new font to project.

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import {
+  useFonts,
+  Nunito_700Bold,
+  Nunito_400Regular,
+} from '@expo-google-fonts/nunito';
+import AppLoading from 'expo-app-loading';
 import { AuthProvider } from './src/context/auth';
 import Routes from './src/routes';
 
 const App: React.FC = () => {
+  let [fontsLoaded] = useFonts({ Nunito_700Bold, Nunito_400Regular });
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+
   return (
     <NavigationContainer>
       <AuthProvider>
@@ -11,6 +23,6 @@ const App: React.FC = () => {
       </AuthProvider>
     </NavigationContainer>
   );
-}
+};
 
 export default App;
