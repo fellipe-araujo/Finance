cf3793dfd3317319ab67bf339cc453cfa7041d69
Fellipe Araujo <araujo.fellipe@hotmail.com>
2021-02-13 02:05:26 -0300

    #21 - Create Artifact Resume component.

---
+++ mobile/src/components/ArtifactResume/styles.ts
@@ -0,0 +1,27 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    width: 160,
+    height: 160,
+    borderRadius: 80,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: -60
+  },
+  title: {
+    fontSize: 20,
+    fontFamily: 'Nunito_400Regular',
+  },
+  subTitle: {
+    fontSize: 16,
+    fontFamily: 'Nunito_400Regular',
+  },
+  value: {
+    marginTop: 10,
+    fontSize: 24,
+    fontFamily: 'Nunito_700Bold',
+  },
+});
+
+export default styles;
