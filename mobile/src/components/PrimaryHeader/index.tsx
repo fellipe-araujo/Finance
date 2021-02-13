afc79ca1b0d7682686ebe5f55549aef0e13feed5
Fellipe Araujo <araujo.fellipe@hotmail.com>
2021-02-13 02:06:05 -0300

    #21 - Create Primary Header component.

---
+++ mobile/src/components/PrimaryHeader/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { View,  Text } from 'react-native';
+import { useIsFocused, useNavigation } from '@react-navigation/native';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
+import { Feather as Icon } from '@expo/vector-icons';
+import styles from './styles';
+
+interface Props {
+  route: string;
+  title: string;
+}
+
+const PrimaryHeader = ({ route, title }: Props) => {
+  const navigation = useNavigation();
+
+  const goToPage = () => {
+    navigation.navigate(`${route}`);
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.content}>
+        <View style={{ width: 30 }} />
+
+        <Text style={styles.title}>{title}</Text>
+
+        <TouchableWithoutFeedback onPress={() => goToPage()}>
+          <Icon name="plus" size={30} color="#FFF" />
+        </TouchableWithoutFeedback>
+      </View>
+    </View>
+  );
+};
+
+export default PrimaryHeader;
