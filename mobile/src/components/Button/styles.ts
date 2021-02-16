9e1255bbe5e04576504e2ee846c0e4e02be2dbfb
Fellipe Araujo <araujo.fellipe@hotmail.com>
2021-02-16 15:24:39 -0300

    #21 Create Button Component.

---
+++ mobile/src/components/Button/styles.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    height: 60,
+    marginVertical: 10,
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    color: '#FFF',
+    fontSize: 24,
+    fontFamily: 'Nunito_700Bold',
+  },
+});
+
+export default styles;
