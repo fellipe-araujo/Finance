7efb0d80169b737c152ac993d8d127981fa8b244
Fellipe Araujo <araujo.fellipe@hotmail.com>
2021-02-16 15:25:08 -0300

    #21 Create Input Component.

---
+++ mobile/src/components/Input/styles.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: 50,
+    backgroundColor: '#FFF',
+    borderRadius: 8,
+    marginTop: 10,
+    borderWidth: 2,
+    padding: 8,
+    color: '#39393A'
+  },
+});
+
+export default styles;
