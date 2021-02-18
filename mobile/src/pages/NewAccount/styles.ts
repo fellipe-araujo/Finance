6a51a9d2f6797ef6add0bf98b9499335def8de39
Fellipe Araujo <araujo.fellipe@hotmail.com>
2021-02-17 21:41:51 -0300

    #21 Add components, logo and connect to backend on NewAccount.

--- mobile/src/pages/NewAccount/styles.ts
+++ mobile/src/pages/NewAccount/styles.ts
@@ -5,6 +5,19 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
   },
+  accountLogo: {
+    marginTop: 20,
+  },
+  content: {
+    flex: 1,
+    width: '80%',
+    justifyContent: 'space-around',
+  },
+  title: {
+    fontSize: 16,
+    fontFamily: 'Nunito_400Regular',
+    color: '#202020'
+  },
 });
 
 export default styles;
