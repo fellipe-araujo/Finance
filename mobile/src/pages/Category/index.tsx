21b5cd433428f44ee70f9aed8c58b9b8a2db4fd1
Fellipe Araujo <araujo.fellipe@hotmail.com>
2021-03-09 10:12:27 -0300

    #22 Create Category page.

--- mobile/src/pages/Category/index.tsx
+++ mobile/src/pages/Category/index.tsx
@@ -1,11 +1,65 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { useIsFocused } from '@react-navigation/native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { ScrollView } from 'react-native';
+import ArtifactResume from '../../components/ArtifactResume';
+import CategoryCard from '../../components/CategoryCard';
+import PrimaryHeader from '../../components/PrimaryHeader';
+import { useAuth } from '../../context/auth';
+import categoryService from '../../services/categoryService';
+import { UserCategory } from '../../utils/types';
 import styles from './styles';
 
-const Category = () => (
-  <View style={styles.container}>
-    <Text>My Categories</Text>
-  </View>
-);
+const Category = () => {
+  const [categories, setCategories] = useState<UserCategory[]>([]);
+
+  const { user } = useAuth();
+
+  const isFocused = useIsFocused();
+
+  useEffect(() => {
+    const fetchAllCategories = async () => {
+      const response = await categoryService.categoryAll(user?._id!);
+
+      setCategories(response);
+    };
+
+    fetchAllCategories();
+  }, [isFocused]);
+
+  return (
+    <LinearGradient
+      style={styles.container}
+      start={{ x: 0.3, y: 0.3 }}
+      end={{ x: 1, y: 1 }}
+      colors={['#B9C0FF', '#42A1DC']}
+    >
+      <PrimaryHeader route="NewCategory" title="Minhas Categorias" />
+
+      <ArtifactResume
+        title="Categorias"
+        subTitle="Totais"
+        value={categories.length.toString()}
+        color1="#F5EC97"
+        color2="#F5F2DA"
+      />
+
+      <ScrollView
+        style={styles.scrollContainer}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ alignItems: 'center' }}
+      >
+        {categories.map((category) => (
+          <CategoryCard
+            key={category._id}
+            categoryId={category._id!}
+            categoryName={category.name!}
+            categoryColor={category.color!}
+          />
+        ))}
+      </ScrollView>
+    </LinearGradient>
+  );
+};
 
 export default Category;
