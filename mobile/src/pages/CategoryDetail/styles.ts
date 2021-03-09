2bfdf33b7023544e3a4cb5c566a8cb6828902a63
Fellipe Araujo <araujo.fellipe@hotmail.com>
2021-03-09 10:13:47 -0300

    #22 Create CategoryDetail page.

---
+++ mobile/src/pages/CategoryDetail/styles.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+
+  content: {
+    flex: 1,
+    width: '80%',
+    justifyContent: 'space-between',
+    marginVertical: 30,
+  },
+});
+
+export default styles;
