99bf4832eef328783e04a372ffbe50dd91db9269
Fellipe Araujo <araujo.fellipe@hotmail.com>
2021-03-09 10:13:08 -0300

    #22 Create NewCategory page.

---
+++ mobile/src/pages/NewCategory/styles.ts
@@ -0,0 +1,23 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+
+  content: {
+    flex: 1,
+    width: '80%',
+  },
+
+  accountLogo: {
+    marginTop: 20,
+  },
+
+  break: {
+    height: 50
+  }
+});
+
+export default styles;
