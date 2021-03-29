cdd308d4e50c0e61225d31472faad13ca9c35160
Fellipe Araujo <araujo.fellipe@hotmail.com>
2021-03-29 20:03:53 -0300

    #29 Create function to update and delete objective and add modal confirm component.

--- mobile/src/pages/ObjectiveDetail/index.tsx
+++ mobile/src/pages/ObjectiveDetail/index.tsx
@@ -1,16 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, ScrollView, TouchableOpacity } from 'react-native';
+import { View, Text, ScrollView, TouchableOpacity, Alert } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
-import { useRoute } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import { useAuth } from '../../context/auth';
 import SecondaryHeader from '../../components/SecondaryHeader';
 import ObjectiveDetailCard from '../../components/ObjectiveDetailCard';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+import ModalConfirm from '../../components/ModalConfirm';
 import styles from './styles';
 import { UserObjective } from '../../utils/types';
 import objectiveService from '../../services/objectiveService';
 import { formatPrice } from '../../utils/formatPrice';
-import Input from '../../components/Input';
-import Button from '../../components/Button';
 
 const NewObjective = () => {
   const [objective, setObjective] = useState<UserObjective>();
@@ -18,11 +19,66 @@ const NewObjective = () => {
   const [newObjectiveDescription, setNewObjectiveDescription] = useState('');
   const [newObjectiveValue, setNewObjectiveValue] = useState('');
   const [optionAdd, setOptionAdd] = useState(true);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [modalAction, setModalAction] = useState('');
 
   const { user } = useAuth();
   const route = useRoute();
 
   const params = route.params as UserObjective;
+
+  const navigation = useNavigation();
+
+  const modalUpdateDescription = `Você deseja atualizar o objetivo ${objective?.name}?`;
+  const modalDeleteDescription = `Você deseja excluir o objetivo ${objective?.name}?`;
+
+  const toggleModalUpdate = async () => {
+    try {
+      var value = 0;
+      if (optionAdd) {
+        value = objective?.amount! + parseInt(newObjectiveValue);
+      } else {
+        value = objective?.amount! - parseInt(newObjectiveValue);
+      }
+
+      const progress = (value / objective?.goal!) * 100;
+      progress.toFixed(2);
+
+      const newObjective: UserObjective = {
+        name: newObjectiveName ? newObjectiveName : objective?.name,
+        description: newObjectiveDescription
+          ? newObjectiveDescription
+          : objective?.description,
+        amount: value,
+        progress,
+      };
+
+      await objectiveService.objectiveUpdate(
+        user?._id!,
+        objective?._id!,
+        newObjective
+      );
+
+      setIsModalVisible(!isModalVisible);
+      navigation.navigate('Objective');
+    } catch (error) {
+      setIsModalVisible(!isModalVisible);
+      navigation.navigate('Objective');
+      Alert.alert(error);
+    }
+  };
+
+  const toggleModalDelete = async () => {
+    try {
+      await objectiveService.objectiveDelete(user?._id!, objective?._id!);
+      setIsModalVisible(!isModalVisible);
+      navigation.navigate('Objective');
+    } catch (error) {
+      setIsModalVisible(!isModalVisible);
+      navigation.navigate('Objective');
+      Alert.alert(error);
+    }
+  };
 
   useEffect(() => {
     const fetchObjective = async () => {
@@ -44,7 +100,20 @@ const NewObjective = () => {
       end={{ x: 1, y: 1 }}
       colors={['#B9C0FF', '#42A1DC']}
     >
-      <SecondaryHeader title={`${objective?.name}`} route="Objective" />
+      <SecondaryHeader title="Detalhe" route="Objective" />
+
+      <ModalConfirm
+        isModalVisible={isModalVisible}
+        toggleModalConfirm={
+          modalAction === 'Update' ? toggleModalUpdate : toggleModalDelete
+        }
+        toggleModalCancel={() => setIsModalVisible(false)}
+        description={
+          modalAction === 'Update'
+            ? modalUpdateDescription
+            : modalDeleteDescription
+        }
+      />
 
       <ScrollView
         style={styles.content}
@@ -121,12 +190,18 @@ const NewObjective = () => {
           <Button
             title="Atualizar Objetivo"
             color="#505050"
-            onPress={() => {}}
+            onPress={() => {
+              setModalAction('Update');
+              setIsModalVisible(true);
+            }}
           />
           <Button
             title="Excluir Objetivo"
             color="#FF8888"
-            onPress={() => {}}
+            onPress={() => {
+              setModalAction('Delete');
+              setIsModalVisible(true);
+            }}
           />
         </View>
       </ScrollView>
