3c2bfb6093215ace014230736ccd048d57153797
Fellipe Araujo <araujo.fellipe@hotmail.com>
2021-04-28 12:27:36 -0300

    #31 Add modal confirm and add function to delete transaction.

--- mobile/src/pages/Transaction/index.tsx
+++ mobile/src/pages/Transaction/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, ScrollView } from 'react-native';
+import { ScrollView, Alert } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { useIsFocused } from '@react-navigation/native';
 import { useAuth } from '../../context/auth';
@@ -7,15 +7,31 @@ import PrimaryHeader from '../../components/PrimaryHeader';
 import ArtifactResume from '../../components/ArtifactResume';
 import TransactionCard from '../../components/TransactionCard';
 import styles from './styles';
+import ModalConfirm from '../../components/ModalConfirm';
 import { UserTransaction } from '../../utils/types';
 import transactionService from '../../services/transactionService';
 
 const Transaction = () => {
   const [transactions, setTransactions] = useState<UserTransaction[]>([]);
+  const [
+    selectedTransaction,
+    setSelectedTransaction,
+  ] = useState<UserTransaction>();
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
   const { user } = useAuth();
 
   const isFocused = useIsFocused();
+
+  const toggleModalDelete = async (transactionId: string) => {
+    try {
+      await transactionService.transactionDelete(user?._id!, transactionId);
+      setIsModalVisible(!isModalVisible);
+    } catch (error) {
+      setIsModalVisible(!isModalVisible);
+      Alert.alert(error);
+    }
+  };
 
   useEffect(() => {
     const fetchAllTransactions = async () => {
@@ -25,7 +41,7 @@ const Transaction = () => {
     };
 
     fetchAllTransactions();
-  }, [isFocused]);
+  }, [isFocused, transactions]);
 
   return (
     <LinearGradient
@@ -36,11 +52,17 @@ const Transaction = () => {
     >
       <PrimaryHeader route="NewTransaction" title="Minhas Transações" />
 
+      <ModalConfirm
+        isModalVisible={isModalVisible}
+        toggleModalConfirm={() => toggleModalDelete(selectedTransaction?._id!)}
+        toggleModalCancel={() => setIsModalVisible(false)}
+        description={`Deseja excluir a transação ${selectedTransaction?.name}?`}
+      />
+
       <ArtifactResume
         title="Transações"
         subTitle="este mês"
         value={transactions.length.toString()}
-        // value={categories.length.toString()}
         color1="#AAF5C8"
         color2="#E5F9ED"
       />
@@ -54,13 +76,16 @@ const Transaction = () => {
           <TransactionCard
             key={transaction._id}
             name={transaction.name!}
-            // name={transaction.name}
             expense={transaction.expense!}
             price={transaction.price!}
             date={transaction.date?.toString()!}
             accountName={transaction.account!.name}
             categoryName={transaction.category!.name!}
             categoryColor={transaction.category!.color!}
+            onDelete={() => {
+              setSelectedTransaction(transaction);
+              setIsModalVisible(true);
+            }}
           />
         ))}
       </ScrollView>
