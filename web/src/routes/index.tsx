054e5420c30432aab759a64d16613b73f0ab8149
Fellipe Araujo <araujo.fellipe@hotmail.com>
2021-05-13 23:53:48 -0300

    #40 Add new route.

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -3,6 +3,7 @@ import Home from '../pages/Home';
 import Accounts from '../pages/Accounts';
 import NewAccount from '../pages/NewAccount';
 import AccountDetail from '../pages/AccountDetail';
+import Objectives from '../pages/Objectives';
 
 const Routes = () => {
   return (
@@ -12,6 +13,7 @@ const Routes = () => {
         <Route path="/accounts" exact component={Accounts} />
         <Route path="/accounts/create" component={NewAccount} />
         <Route path="/accounts/:id" component={AccountDetail} />
+        <Route path="/objectives" component={Objectives} />
       </Switch>
     </BrowserRouter>
   );
