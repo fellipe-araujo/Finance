594158dd3358a395a92394efd3113f30ec6467c9
Fellipe Araujo <araujo.fellipe@hotmail.com>
2021-05-15 01:01:26 -0300

    #40 Create New Objective page.

---
+++ web/src/pages/NewObjective/styles.ts
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Content = styled.div`
+  width: 100%;
+  height: 100%;
+  margin: 5rem 0;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+
+  .new-objective-image {
+    width: 25rem;
+  }
+`;
+
+
+const Options = styled.div`
+  width: 80%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export { Container, Content, Options };
