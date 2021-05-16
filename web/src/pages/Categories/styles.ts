991cd71c650105d58c050b560ce9ff63b5f9c385
Fellipe Araujo <araujo.fellipe@hotmail.com>
2021-05-16 12:05:26 -0300

    #40 Create Categories page.

---
+++ web/src/pages/Categories/styles.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const List = styled.div`
+  width: 90%;
+  margin-top: 15rem;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .accounts-link {
+    text-decoration: none;
+  }
+`;
+
+export { Container, List };
