262e8390ab0dba8054c77e71f45b9f9020d7729c
Fellipe <araujo.fellipe@hotmail.com>
2021-05-30 12:28:51 -0300

    #45 Config public and private routes.

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,39 +1,41 @@
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Home from '../pages/Home';
-import Accounts from '../pages/Accounts';
-import NewAccount from '../pages/NewAccount';
-import AccountDetail from '../pages/AccountDetail';
-import Objectives from '../pages/Objectives';
-import NewObjective from '../pages/NewObjective';
-import ObjectiveDetail from '../pages/ObjectiveDetail';
-import Categories from '../pages/Categories';
-import NewCategory from '../pages/NewCategory';
-import CategoryDetail from '../pages/CategoryDetail';
-import Transactions from '../pages/Transactions';
-import NewTransaction from '../pages/NewTransaction';
+import { Switch, Route } from "react-router-dom";
+import PrivateRoute from "../components/PrivateRoute";
+import Login from "../pages/Login";
+import Home from "../pages/Home";
+import Accounts from "../pages/Accounts";
+import NewAccount from "../pages/NewAccount";
+import AccountDetail from "../pages/AccountDetail";
+import Objectives from "../pages/Objectives";
+import NewObjective from "../pages/NewObjective";
+import ObjectiveDetail from "../pages/ObjectiveDetail";
+import Categories from "../pages/Categories";
+import NewCategory from "../pages/NewCategory";
+import CategoryDetail from "../pages/CategoryDetail";
+import Transactions from "../pages/Transactions";
+import NewTransaction from "../pages/NewTransaction";
 
 const Routes = () => {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" exact component={Home} />
+    <Switch>
+      <PrivateRoute path="/" exact component={Home} />
 
-        <Route path="/accounts" exact component={Accounts} />
-        <Route path="/accounts/create" component={NewAccount} />
-        <Route path="/accounts/:id" component={AccountDetail} />
+      <PrivateRoute path="/accounts" exact component={Accounts} />
+      <PrivateRoute path="/accounts/create" component={NewAccount} />
+      <PrivateRoute path="/accounts/:id" component={AccountDetail} />
 
-        <Route path="/objectives" exact component={Objectives} />
-        <Route path="/objectives/create" component={NewObjective} />
-        <Route path="/objectives/:id" component={ObjectiveDetail} />
+      <PrivateRoute path="/objectives" exact component={Objectives} />
+      <PrivateRoute path="/objectives/create" component={NewObjective} />
+      <PrivateRoute path="/objectives/:id" component={ObjectiveDetail} />
 
-        <Route path="/categories" exact component={Categories} />
-        <Route path="/categories/create" component={NewCategory} />
-        <Route path="/categories/:id" component={CategoryDetail} />
+      <PrivateRoute path="/categories" exact component={Categories} />
+      <PrivateRoute path="/categories/create" component={NewCategory} />
+      <PrivateRoute path="/categories/:id" component={CategoryDetail} />
 
-        <Route path="/transactions" exact component={Transactions} />
-        <Route path="/transactions/create" component={NewTransaction} />
-      </Switch>
-    </BrowserRouter>
+      <PrivateRoute path="/transactions" exact component={Transactions} />
+      <PrivateRoute path="/transactions/create" component={NewTransaction} />
+
+      <Route path="/login" exact component={Login} />
+    </Switch>
   );
 };
 
