9c6b0effc40205a0e7e5f37adfba86d2501c41a5
Fellipe <araujo.fellipe@hotmail.com>
2021-05-31 12:38:26 -0300

    #45 Create transaction service.

---
+++ web/src/services/transactionService.ts
@@ -0,0 +1,32 @@
+import api from "./api";
+import { UserTransaction } from "../utils/types";
+
+class TransactionService {
+  async transactionCreate(
+    userId: string,
+    accountId: string,
+    data: UserTransaction
+  ) {
+    await api.post(`/users/${userId}/accounts/${accountId}/transactions`, data);
+  }
+
+  async transactionAll(userId: string) {
+    const transactions = await api.get(`/users/${userId}/transactions/all`);
+    return transactions.data;
+  }
+
+  async transactionOne(userId: string, transactionId: string) {
+    const transaction = await api.get(
+      `/users/${userId}/transactions/${transactionId}`
+    );
+    return transaction.data;
+  }
+
+  async transactionDelete(userId: string, transactionId: string) {
+    await api.delete(`/users/${userId}/transactions/${transactionId}/delete`);
+  }
+}
+
+const transactionService = new TransactionService();
+
+export default transactionService;
