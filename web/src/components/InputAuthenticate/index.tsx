27f83d436deaccb64b03f19c5e2acb5d88bd9452
Fellipe <araujo.fellipe@hotmail.com>
2021-06-01 11:01:15 -0300

    #45 Add value and onChange props in inputs.

--- web/src/components/InputAuthenticate/index.tsx
+++ web/src/components/InputAuthenticate/index.tsx
@@ -1,15 +1,18 @@
-import { Container, Input } from './styles';
+import React from 'react';
+import { Container, Input } from "./styles";
 
 interface InputProps {
   title: string;
   type: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const InputAuthenticate = ({ title, type }: InputProps) => {
+const InputAuthenticate = ({ title, type, value, onChange }: InputProps) => {
   return (
     <Container>
       <h2 className="input-authenticate-title">{title}</h2>
-      <Input required type={type} />
+      <Input required type={type} value={value} onChange={onChange} />
     </Container>
   );
 };
