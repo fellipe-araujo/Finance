8f9a99d9b1f4eca7df1e91101183e2ed03fbcabb
Fellipe <araujo.fellipe@hotmail.com>
2021-06-01 12:36:44 -0300

    #45 Update category card structure and generate category styles and structure.

--- web/src/components/CategoryCard/styles.ts
+++ web/src/components/CategoryCard/styles.ts
@@ -1,21 +1,15 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const Container = styled.div`
-  width: 70%;
+  width: 100%;
   height: 5rem;
   border-radius: 0.8rem;
   margin: 1.5rem 0;
   background: ${(props) => props.color};
 
-  .category-card-link {
-    width: 100%;
-    height: 100%;
-    text-decoration: none;
-
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
   .category-card-title {
     font-size: 2.4rem;
