701a9bb1d8c4da0ed8731c35e8d07aa1756306e3
Fellipe <araujo.fellipe@hotmail.com>
2021-06-01 11:00:16 -0300

    #45 Do header dynamic.

--- web/src/components/Header/index.tsx
+++ web/src/components/Header/index.tsx
@@ -1,11 +1,15 @@
-import { useState } from 'react';
-import { HeaderContainer, Menu } from './styles';
-import { FiMenu } from 'react-icons/fi';
-import Modal from 'react-modal';
-import { Link } from 'react-router-dom';
-import Logo from '../../assets/Logo.svg';
+import { useState } from "react";
+import { HeaderContainer, Menu } from "./styles";
+import { FiMenu } from "react-icons/fi";
+import Modal from "react-modal";
+import { Link } from "react-router-dom";
+import Logo from "../../assets/Logo.svg";
 
-const Header = () => {
+interface HeaderProps {
+  name: string;
+}
+
+const Header = ({ name }: HeaderProps) => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
 
   const openModal = () => {
@@ -23,23 +27,23 @@ const Header = () => {
         onRequestClose={closeModal}
         style={{
           overlay: {
-            backgroundColor: 'rgba(0, 0, 0, 0.75)',
+            backgroundColor: "rgba(0, 0, 0, 0.75)",
           },
           content: {
-            width: '65%',
-            top: '50%',
-            left: '50%',
-            right: 'auto',
-            bottom: 'auto',
-            marginRight: '-50%',
-            transform: 'translate(-50%, -50%)',
-            border: '0.1rem solid #ccc',
-            background: '#fff',
-            overflow: 'auto',
-            WebkitOverflowScrolling: 'touch',
-            borderRadius: '4px',
-            outline: 'none',
-            padding: '2rem',
+            width: "65%",
+            top: "50%",
+            left: "50%",
+            right: "auto",
+            bottom: "auto",
+            marginRight: "-50%",
+            transform: "translate(-50%, -50%)",
+            border: "0.1rem solid #ccc",
+            background: "#fff",
+            overflow: "auto",
+            WebkitOverflowScrolling: "touch",
+            borderRadius: "4px",
+            outline: "none",
+            padding: "2rem",
           },
         }}
         contentLabel="Escolha um opção"
@@ -61,7 +65,7 @@ const Header = () => {
         </Menu>
       </Modal>
       <img src={Logo} alt="Finance Logo" />
-      <h1>Fellipe</h1>
+      <h1>{name}</h1>
       <button onClick={() => openModal()}>
         <FiMenu size={30} color="#FFF" />
       </button>
