4e30501c6ee8ca85150f764f910945f740035fc4
Fellipe <araujo.fellipe@hotmail.com>
2021-06-10 17:14:01 -0300

    Create chart config.

---
+++ web/src/utils/chartConfig.ts
@@ -0,0 +1,20 @@
+export const chartConfig = {
+  options: {
+    chart: {
+      background: "transparent",
+    },
+    dataLabels: {
+      enabled: true,
+      style: {
+        colors: ["#202020"],
+        fontWeight: 400,
+      },
+    },
+    stroke: {
+      show: false,
+    },
+    legend: {
+      show: false,
+    },
+  },
+};
