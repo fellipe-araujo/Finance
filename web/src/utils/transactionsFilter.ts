df76983c307bc9f1a73562cac5c8a6e25b8e40d2
Fellipe <araujo.fellipe@hotmail.com>
2021-07-05 17:55:18 -0300

    Create function to fetch transactions by month and year.

--- web/src/utils/transactionsFilter.ts
+++ web/src/utils/transactionsFilter.ts
@@ -1,12 +1,38 @@
-import transactionService from "../services/transactionService";
-import { User, UserTransaction } from "./types";
+import transactionService from '../services/transactionService';
+import { period } from './period';
+import { User, UserTransaction } from './types';
+
+interface MonthProps {
+  id: string;
+  value: string;
+}
 
 export const fetchAllTransactions = async (user: User) => {
   const allTransactions = await transactionService.transactionAll(user?._id!);
 
   return {
     transactions: allTransactions.reverse(),
-    type: "totais",
+    type: 'totais',
+  };
+};
+
+export const fetchMonthAndYearTransactions = async (
+  user: User,
+  month: string,
+  year: string
+) => {
+  const allTransactions = await transactionService.transactionAll(user?._id!);
+
+  const selectedMonthAndYearTransactions: UserTransaction[] =
+    allTransactions.filter(
+      (transaction: UserTransaction) =>
+        transaction.date?.toLocaleString('pt-BR').slice(0, 7) ===
+        `${year}-${month}`
+    );
+
+  return {
+    transactions: selectedMonthAndYearTransactions.reverse(),
+    type: period.months.find((item: MonthProps) => item.id === month)?.value,
   };
 };
 
@@ -14,10 +40,10 @@ export const fetchAllEntriesTransactions = async (user: User) => {
   const allTransactions = await transactionService.transactionAll(user?._id!);
 
   return {
-    transactions: allTransactions.filter(
-      (transaction: UserTransaction) => transaction.expense === false
-    ).reverse(),
-    type: "entradas",
+    transactions: allTransactions
+      .filter((transaction: UserTransaction) => transaction.expense === false)
+      .reverse(),
+    type: 'entradas',
   };
 };
 
@@ -25,28 +51,28 @@ export const fetchAllExpensesTransactions = async (user: User) => {
   const allTransactions = await transactionService.transactionAll(user?._id!);
 
   return {
-    transactions: allTransactions.filter(
-      (transaction: UserTransaction) => transaction.expense === true
-    ).reverse(),
-    type: "saídas",
+    transactions: allTransactions
+      .filter((transaction: UserTransaction) => transaction.expense === true)
+      .reverse(),
+    type: 'saídas',
   };
 };
 
 export const fetchCurrentMonthTransactions = async (user: User) => {
   const currentDate = new Date();
-  const currentMonth = currentDate.toLocaleString("pt-BR").slice(3, 5);
-  const currentYear = currentDate.toLocaleString("pt-BR").slice(6, 10);
+  const currentMonth = currentDate.toLocaleString('pt-BR').slice(3, 5);
+  const currentYear = currentDate.toLocaleString('pt-BR').slice(6, 10);
 
   const allTransactions = await transactionService.transactionAll(user?._id!);
 
   const currentMonthTransactions: UserTransaction[] = allTransactions.filter(
     (transaction: UserTransaction) =>
-      transaction.date?.toLocaleString("pt-BR").slice(0, 7) ===
+      transaction.date?.toLocaleString('pt-BR').slice(0, 7) ===
       `${currentYear}-${currentMonth}`
   );
 
   return {
     transactions: currentMonthTransactions.reverse(),
-    type: "este mês",
+    type: 'este mês',
   };
 };
