98a71e09a4a9796fa500a3853d16c5d4fd573427
Fellipe <araujo.fellipe@hotmail.com>
2021-07-05 17:49:41 -0300

    Add period data.

---
+++ web/src/utils/period.ts
@@ -0,0 +1,70 @@
+export const period = {
+  years: [
+    {
+      id: '01',
+      value: '2021',
+    },
+    {
+      id: '02',
+      value: '2022',
+    },
+    {
+      id: '03',
+      value: '2023',
+    },
+    {
+      id: '04',
+      value: '2024',
+    },
+  ],
+  months: [
+    {
+      id: '01',
+      value: 'Janeiro',
+    },
+    {
+      id: '02',
+      value: 'Fevereiro',
+    },
+    {
+      id: '03',
+      value: 'Março',
+    },
+    {
+      id: '04',
+      value: 'Abril',
+    },
+    {
+      id: '05',
+      value: 'Maio',
+    },
+    {
+      id: '06',
+      value: 'Junho',
+    },
+    {
+      id: '07',
+      value: 'Julho',
+    },
+    {
+      id: '08',
+      value: 'Agosto',
+    },
+    {
+      id: '09',
+      value: 'Setembro',
+    },
+    {
+      id: '10',
+      value: 'Outubro',
+    },
+    {
+      id: '11',
+      value: 'Novembro',
+    },
+    {
+      id: '12',
+      value: 'Dezembro',
+    },
+  ],
+};
