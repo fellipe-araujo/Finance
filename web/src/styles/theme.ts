0a7e8850a577dbc55bb7527d83c7778de38f9cf4
Fellipe <araujo.fellipe@hotmail.com>
2021-09-09 22:20:26 -0300

    Added new colors palette.

---
+++ web/src/styles/theme.ts
@@ -0,0 +1,22 @@
+export const theme = {
+  colors: {
+    purpleLight: '#7F13D4',
+    purpleMedium: '#6514A4',
+    purpleDark: '#4C0783',
+    grayLight: '#8B8B8B',
+    grayMedium: '#4B4B4F',
+    grayDark: '#39393A',
+    greenLight: '#89FF87',
+    greenDark: '#40923F',
+    redLight: '#FF8888',
+    redDark: '#BB4E4E',
+    yellowLight: '#FFF973',
+    yellowDark: '#BCAF35',
+    artifactLight: '#40B1CA',
+    artifactMedium: '#15B3D6',
+    artifactDark: '#0F6A7E',
+    financeBlue: '#31C6E8',
+    white: '#FFFFFF',
+    black: '#000000',
+  },
+}
