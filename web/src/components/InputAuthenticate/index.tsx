d3743ac66e736b3a3eeeaee02c4c7d30facc9daa
Fellipe <araujo.fellipe@hotmail.com>
2021-09-13 10:52:03 -0300

    #52 Refactored authenticate input component.

--- web/src/components/InputAuthenticate/index.tsx
+++ web/src/components/InputAuthenticate/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Input } from "./styles";
+import { Container, Title, Input } from "./styles";
 
 interface InputProps {
   title: string;
@@ -11,7 +11,7 @@ interface InputProps {
 const InputAuthenticate = ({ title, type, value, onChange }: InputProps) => {
   return (
     <Container>
-      <h2 className="input-authenticate-title">{title}</h2>
+      <Title>{title}</Title>
       <Input required type={type} value={value} onChange={onChange} />
     </Container>
   );
