c9e9af5504b91b76c15b51201c4fcbba66d268f6
Fellipe <araujo.fellipe@hotmail.com>
2021-09-13 22:44:42 -0300

    #55 Refactored secondary header component.

--- web/src/components/SecondaryHeader/styles.ts
+++ web/src/components/SecondaryHeader/styles.ts
@@ -1,12 +1,10 @@
-import styled from "styled-components";
-import { colors } from "../../styles/colors";
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 const Container = styled.div`
   width: 100%;
-  height: 10rem;
-  border-bottom-left-radius: 3rem;
-  border-bottom-right-radius: 3rem;
-  background-color: ${colors.grayMedium};
+  height: 12rem;
+  background-color: ${({ theme }) => theme.colors.purpleMedium};
   padding: 0 2rem;
 
   display: flex;
@@ -14,20 +12,23 @@ const Container = styled.div`
   justify-content: space-between;
   align-items: center;
 
-  .secondary-header-link {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
+  position: fixed;
+  top: 0;
 
-  .secondary-header-title {
-    color: ${colors.white};
-    font-size: 2.6rem;
-  }
-
-  .secondary-header-invisible {
+  > div {
     width: 3rem;
   }
 `;
 
-export { Container };
+const LinkGoBack = styled(Link)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Title = styled.h2`
+  color: ${({ theme }) => theme.colors.white};
+  font-size: ${({ theme }) => theme.fonts.size.title};
+`;
+
+export { Container, LinkGoBack, Title };
