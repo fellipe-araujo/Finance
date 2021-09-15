a82cba9de5985f06f4365e719d4f05203720738c
Fellipe <araujo.fellipe@hotmail.com>
2021-09-15 09:01:18 -0300

    #62 Refactored transaction filter modal component.

--- web/src/components/ModalTransactionFilter/styles.ts
+++ web/src/components/ModalTransactionFilter/styles.ts
@@ -1,43 +1,46 @@
-import styled from "styled-components";
-import { colors } from "../../styles/colors";
+import styled from 'styled-components';
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
 
-  .modal-filter-logo {
+  > img {
     width: 5rem;
-  }
-
-  .modal-filter-button {
-    width: 100%;
-    background: transparent;
-    border: 0;
-    color: ${colors.grayMedium};
-    margin: 1rem 0;
-
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
-
-  .modal-filter-title {
-    margin: 2rem 0 1rem;
-    font-size: 2.2rem;
-    text-align: center;
-  }
-
-  .modal-filter-option {
-    font-size: 2rem;
-  }
-
-  .modal-filter-line {
-    width: 80%;
-    height: 0.1rem;
-    background-color: ${colors.grayMedium};
-    opacity: 0.2;
   }
 `;
 
-export { Container };
+const Title = styled.h2`
+  margin: 2rem 0 1rem;
+  color: ${({ theme }) => theme.colors.white};
+  font-size: ${({ theme }) => theme.fonts.size.title};
+  font-weight: ${({ theme }) => theme.fonts.weight.light};
+  text-align: center;
+`;
+
+const ButtonOption = styled.button`
+  width: 100%;
+  background: transparent;
+  border: 0;
+  color: ${({ theme }) => theme.colors.grayMedium};
+  margin: 1rem 0;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const SubTitle = styled.h2`
+  color: ${({ theme }) => theme.colors.grayLight};
+  font-size: 1.4rem;
+  font-weight: ${({ theme }) => theme.fonts.weight.light};
+`;
+
+const Separator = styled.div`
+  width: 80%;
+  height: 0.1rem;
+  background-color: ${({ theme }) => theme.colors.grayMedium};
+  opacity: 0.2;
+`;
+
+export { Container, Title, ButtonOption, SubTitle, Separator };
